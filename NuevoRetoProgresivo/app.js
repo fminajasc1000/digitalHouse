3beb3102738ec920a43f8ab7a30cdcd7bf61797c
franciscominajas <franciscominajas@gmail.com>
2021-11-06 20:57:36 -0600

    RETO 2 COMPLETADO

--- NuevoRetoProgresivo/app.js
+++ NuevoRetoProgresivo/app.js
@@ -1,36 +1,11 @@
-const path = require('path');
 const express = require('express');
 const app = express();
+const path = require('path');
+
 app.listen(3030, () => console.log('Servidor Arriba'));
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/index.html'));
-});
+app.use(express.static('public'));
 
-app.get('/babbage', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/babbage.html'));
-});
-
-app.get('/berners-lee', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/berners-lee.html'));
-});
-
-app.get('/clarke', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/clarke.html'));
-});
-
-app.get('/hamilton', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/hamilton.html'));
-});
-
-app.get('/hopper', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/hopper.html'));
-});
-
-app.get('/lovelace', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/lovelace.html'));
-});
-
-app.get('/turing', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/turing.html'));
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/home.html'));
 });
